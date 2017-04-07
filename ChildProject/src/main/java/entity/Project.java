ea85e24fc7cb18436d116a9929ccbbf380262337
vserdiuk <vitaliy.serdiuk@gmail.com>
2017-04-07 22:53:48 +0300

    first commit

---
+++ ChildProject/src/main/java/entity/Project.java
@@ -0,0 +1,38 @@
+package entity;
+
+/**
+ * Created by vserdiuk on 4/7/17.
+ */
+public class Project {
+
+    private Long id;
+    private String title;
+
+    public Project() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
---
+++ ModuleBL/src/main/java/entity/Employee.java
@@ -0,0 +1,48 @@
+package entity;
+
+/**
+ * Created by vserdiuk on 4/7/17.
+ */
+public class Employee {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Employee() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
